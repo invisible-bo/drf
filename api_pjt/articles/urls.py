b0142058a1f647813f14b522d7bfb867c893aa79
invisible-bo <qhrms111@gmail.com>
2025-02-03 03:23:31 +0900

    0203

--- api_pjt/articles/urls.py
+++ api_pjt/articles/urls.py
@@ -3,5 +3,6 @@ from.import views
 
 app_name = 'articles'
 urlpatterns = [
-    path("", views.article_list, name="article_list"),
+    path("", views.article_list),
+    path("<int:pk>/", views.article_detail),
 ]
--- api_pjt/articles/views.py
+++ api_pjt/articles/views.py
@@ -3,11 +3,27 @@ from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Article
 from .serializers import ArticleSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
+    if request.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=201)
+   
+
+
+@api_view(['GET'])
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    serializer = ArticleSerializer(article)
     return Response(serializer.data)
